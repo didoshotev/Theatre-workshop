0f3ca1f93fbceb42951e7c5a2edee970623d1098
Didoshotev <didoshotev@gmail.com>
2020-09-25 17:31:18 +0300

    feat/routes and views

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const env = process.env.NODE_ENV;
+
+const config = require('./config/config')[env];
+const mognoose = require('mongoose');
+const express = require('express');
+const app = express();
+const DB_URL = process.env.DATABASE_URL;
+const mainRouter = require('./routes/main');
+
+mognoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+}, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    };
+    console.log('Database is setup and running!');
+});
+require('./config/express')(app)
+mainRouter(app);
+
+app.listen(config.port, console.log(`Listening on port ${config.port}!`))
